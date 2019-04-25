dfb803a7429ac5bc3ae7e4389b7e37a3556155a7
Mauricio Sabogal <mauricio@kommit.co>
2019-04-25 11:21:52 -0500

    BEL-70 Move compile code to compile_from_source.sh

--- scripts/build_nginx.sh
+++ scripts/build_nginx.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 # Build NGINX and modules on Heroku.
-# This program is designed to run in a web dyno provided by Heroku.
-# We would like to build an NGINX binary for the builpack on the
-# exact machine in which the binary will run.
-# Our motivation for running in a web dyno is that we need a way to
-# download the binary once it is built so we can vendor it in the buildpack.
+# When deployed as 'an app', this Procfile will be executed and the
+# compiled Nginx binary will be available to download from the root web page
+#
+# Before deployment, delete the binary for your Heroku stack so that the
+# script will build a new one.  If it finds a binary in /bin, it won't build new.
 #
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
-
-NGINX_VERSION=${NGINX_VERSION-1.9.10}
-PCRE_VERSION=${PCRE_VERSION-8.38}
-HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
-OPEN_SSL_VERSION=${OPEN_SSL_VERSION-1.0.1p}
-
-nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-pcre_tarball_url=http://downloads.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
-headers_more_nginx_module_url=https://github.com/agentzh/headers-more-nginx-module/archive/v${HEADERS_MORE_VERSION}.tar.gz
-open_ssl_url=https://www.openssl.org/source/openssl-${OPEN_SSL_VERSION}.tar.gz
+#
+# Once downloaded, rename it to match the Heroku stack name and copy it into the
+# buildpack's /bin directory.  Mark it as executable with 'chmod 744 nginx-STACKNAME'
+# then commit and push to GitHub.  Now your buildpack will be useable on that stack
+# without paying the download/compile time penalty.
 
 temp_dir=$(mktemp -d /tmp/nginx.XXXXXXXXXX)
 
@@ -25,30 +20,7 @@ echo "Serving files from /tmp on $PORT"
 cd /tmp
 python -m SimpleHTTPServer $PORT &
 
-cd $temp_dir
-echo "Temp dir: $temp_dir"
-
-echo "Downloading $nginx_tarball_url"
-curl -L $nginx_tarball_url | tar xzv
-
-echo "Downloading $pcre_tarball_url"
-(cd nginx-${NGINX_VERSION} && curl -L $pcre_tarball_url | tar xvj )
-
-echo "Downloading $headers_more_nginx_module_url"
-(cd nginx-${NGINX_VERSION} && curl -L $headers_more_nginx_module_url | tar xvz )
-
-echo "Downloading $open_ssl_url"
-(cd nginx-${NGINX_VERSION} && curl -L $open_ssl_url | tar xvz )
-
-(
-	cd nginx-${NGINX_VERSION}
-	./configure \
-		--with-pcre=pcre-${PCRE_VERSION} \
-		--prefix=/tmp/nginx \
-		--add-module=${temp_dir}/nginx-${NGINX_VERSION}/headers-more-nginx-module-${HEADERS_MORE_VERSION} \
-    --with-http_ssl_module --with-openssl=${temp_dir}/nginx-${NGINX_VERSION}/openssl-${OPEN_SSL_VERSION}
-	make install
-)
+cp ~/bin/nginx "/tmp/nginx"
 
 while true
 do
