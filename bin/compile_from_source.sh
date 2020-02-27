c4e3e73f1409117baf1d500703df505c5ff6a9f7
Mauricio Sabogal <mauricio@kommit.co>
2020-02-27 17:38:48 -0500

    BEL-70 Update nginx version for heroku-18

--- bin/compile_from_source.sh
+++ bin/compile_from_source.sh
@@ -9,10 +9,10 @@
 # Once the dyno has is 'up' you can open your browser and navigate
 # this dyno's directory structure to download the nginx binary.
 
-NGINX_VERSION=${NGINX_VERSION-1.9.10}
+NGINX_VERSION=${NGINX_VERSION-1.16.1}
 PCRE_VERSION=${PCRE_VERSION-8.38}
 HEADERS_MORE_VERSION=${HEADERS_MORE_VERSION-0.23}
-OPEN_SSL_VERSION=${OPEN_SSL_VERSION-1.0.1p}
+OPEN_SSL_VERSION=${OPEN_SSL_VERSION-1.1.1}
 
 nginx_tarball_url=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 pcre_tarball_url=http://downloads.sourceforge.net/project/pcre/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.bz2
